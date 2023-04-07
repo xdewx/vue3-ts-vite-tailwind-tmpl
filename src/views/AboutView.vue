4493c0217e2db5aa8d0a8021b084ea74d84f2bf9
leoking <present150608@sina.com>
2023-04-07 16:16:51 +0800

    feat: add tailwind and prettier-tailwind

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <div class="flex flex-col">
+      <div class="bottom-2">tailwindcss</div>
+      <div class="flex-1 border">hello tailwindcss</div>
+    </div>
   </div>
 </template>
 
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/*
+ * @Author: leoking
+ * @Date: 2023-04-07 15:39:06
+ * @LastEditTime: 2023-04-07 15:59:46
+ * @LastEditors: leoking
+ * @Description: 
+ */
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  // prefix:'tw-',
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
