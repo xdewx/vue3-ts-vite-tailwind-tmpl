50e8358648ac0aaf11d097a1425d794a1933946e
leoking <present150608@sina.com>
2023-04-07 17:10:00 +0800

    feat(ui): 增加naive并配置自动引入

--- vite.config.ts
+++ vite.config.ts
@@ -6,15 +6,31 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import Icons from 'unplugin-icons/vite'
+import { NaiveUiResolver } from 'unplugin-vue-components/resolvers'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(), vueJsx(),
     AutoImport({
-      imports: ['vue', 'vue-router', 'pinia', '@vueuse/core', '@vueuse/head', ],
+      imports: [
+        'vue',
+        'vue-router',
+        'pinia',
+        '@vueuse/core', '@vueuse/head',
+        {
+          'naive-ui': [
+            'useDialog',
+            'useMessage',
+            'useNotification',
+            'useLoadingBar'
+          ]
+        }
+      ]
     }),
-    Components({}),
+    Components({
+      resolvers:[NaiveUiResolver(),],
+    }),
     Icons({}),
   ],
   resolve: {
